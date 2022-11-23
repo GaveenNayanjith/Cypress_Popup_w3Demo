3a0caefb6d4f66a7131a47a0fe0f4cc883adb1b9
Gaveen Nayanajith <73846334+GaveenNayanjith@users.noreply.github.com>
2022-11-24 01:43:26 +0530

    Update alertHandling.cy.js

--- cypress/e2e/1-getting-started/alertHandling.cy.js
+++ cypress/e2e/1-getting-started/alertHandling.cy.js
@@ -9,4 +9,10 @@ it('Handling alert in Cypress', function (){
 
   //Accessing the popup button iframe and clicking the popup activation button
   cy.xpath('//iframe[@id="iframeResult"]').its('0.contentDocument.body').then(cy.wrap).xpath('//button[text()="Try it"]').click();
+
+  //Checking the window alert text
+  cy.on('window:alert',(txt)=>{
+    //Assertion
+    expect(txt).to.contains('I am an alert box!');
+ })
 });
